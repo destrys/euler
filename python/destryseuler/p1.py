13ee6f62101b38f6f995669b36ab881d81601d00
Destry Saul <destrys@gmail.com>
2017-10-07 13:19:05 -0700

    setting up p1 test

--- python/destryseuler/p1.py
+++ python/destryseuler/p1.py
@@ -1,3 +1,14 @@
 
-def answer():
-    pass
+def natural3and5Brute(upper=1000):
+    output = 0
+    for i in range(1,upper):
+        if i % 3 == 0:
+            output += i
+            continue
+        if i % 5 == 0:
+            output += i
+            continue
+    return output
+
+def answer(n):
+    return natural3and5Brute(n)
--- python/destryseuler/tests/test_p1.py
+++ python/destryseuler/tests/test_p1.py
@@ -1,4 +1,4 @@
 from destryseuler import p1
 
 def test_p1_answer():
-    assert True
+    assert p1.answer(10) == 23
