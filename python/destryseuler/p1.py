e948d85c721d81698be9e73b9017a4ef539ee21c
Destry Saul <destrys@gmail.com>
2017-10-07 13:58:56 -0700

    some notebook fun

--- python/destryseuler/p1.py
+++ python/destryseuler/p1.py
@@ -1,5 +1,10 @@
+import math
 
-def natural3and5Brute(upper=1000):
+def answer(n):
+    return natural_3and5(n)
+
+
+def natural_3and5_brute(upper=1000):
     output = 0
     for i in range(1,upper):
         if i % 3 == 0:
@@ -10,5 +15,19 @@ def natural3and5Brute(upper=1000):
             continue
     return output
 
-def answer(n):
-    return natural3and5Brute(n)
+
+def natural_3and5(n=1000):
+    floor3 = math.floor((n-1)/3)
+    floor5 = math.floor((n-1)/5)
+    floor15 = math.floor((n-1)/15)
+    term1 = 3 * (floor3 * (floor3 + 1)) / 2
+    term2 = 5 * (floor5 * (floor5 + 1)) / 2
+    term3 = 15 * (floor15 * (floor15 + 1)) / 2
+    output = term1 + term2 + term3
+    return output
+
+def natural_3and5_lambda(n=1000):
+    f = lambda x: math.floor((n - 1) / x)
+    t = lambda x: x * (f(x) * (f(x) + 1)) / 2
+    output = t(3) + t(5) - t(15)
+    return output
--- python/destryseuler/tests/test_p1.py
+++ python/destryseuler/tests/test_p1.py
@@ -2,3 +2,10 @@ from destryseuler import p1
 
 def test_p1_answer():
     assert p1.answer(10) == 23
+
+def test_brute():
+    assert p1.natural_3and5_brute(10) == 23
+
+def test_lambda():
+    assert p1.natural_3and5_lambda(10) == 23
+    assert p1.natural_3and5_lambda(1000) == 233168
